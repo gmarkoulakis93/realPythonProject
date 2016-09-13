bf7fdd28b45970256237064a2d833f9f85995ec9
George Markoulakis <georgemarkoulakis2015@u.northwestern.edu>
2016-09-12 20:42:03 -0700

    adding config stuff

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -10,6 +14,8 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+print(os.environ['APP_SETTINGS'])
 
 if __name__ == '__main__':
     app.run()
